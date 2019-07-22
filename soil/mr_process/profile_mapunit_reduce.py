165a2f51bef6902bfcfe72bbcac58bc77ba121c5
Gary Ng <nyjgary@gmail.com>
2019-07-21 21:21:35 -0400

    handle null in parsing val

--- soil/mr_process/profile_mapunit_reduce.py
+++ soil/mr_process/profile_mapunit_reduce.py
@@ -8,15 +8,15 @@ cur_field, cur_min, cur_max, cur_null = None, 99999, -99999, 0
 # process each key/value pair from map output 
 for line in sys.stdin: 
 	l = line.strip().split("\t")
-	field, val = l[0], int(l[1])
+	field, val = l[0], l[1]
 
 	# if incoming field is the current field, update summary statistics 
 	if field == cur_field: 
 		if val == '':
 			cur_null += 1 
 		else: 
-			cur_min = min(cur_min, val)
-			cur_max = max(cur_max, val)
+			cur_min = min(cur_min, float(val))
+			cur_max = max(cur_max, float(val))
 
 	# else it must be a new field, and we must have finished the previous field 
 	else: 
@@ -29,7 +29,7 @@ for line in sys.stdin:
 			cur_null = 1 
 		else: 
 			cur_null = 0
-			cur_min, cur_max = val, val 
+			cur_min, cur_max = float(val), float(val) 
 
 # handle the last key 
 print("%s\t%s\t%s\t%s" % (cur_field, cur_min, cur_max, cur_null))
