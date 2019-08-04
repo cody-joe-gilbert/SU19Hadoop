9cbf58b7d3080cfd1724aaaac8700e8db35b4e6c
Gary Ng <nyjgary@gmail.com>
2019-08-04 14:29:59 -0400

    use Napa-2010 for weather comparison

--- composite/wrangle_weather_sim.sql
+++ composite/wrangle_weather_sim.sql
@@ -18,19 +18,22 @@ LOAD DATA INPATH 'hdfs:/user/yjn214/rbda-proj/weatherSim1_v2' OVERWRITE INTO TAB
 LOAD DATA INPATH 'hdfs:/user/yjn214/rbda-proj/weatherSim2_v2' OVERWRITE INTO TABLE weather_sim_2;
 
 # wrangle weather sim data from long- to wide-format, with similarities for each decade as separate columns  
+# note that we are comparing each station's similarity with Napa-2010
 
 CREATE TABLE weather_sim AS 
 	SELECT t1.station_id, weather_sim_2000, weather_sim_2010, weather_sim_future
 	FROM (
 		SELECT station_id, 
-			AVG(CASE WHEN station_decade = '200' AND napa_decade = '2000' THEN weather_sim ELSE NULL END) AS weather_sim_2000, 
-			AVG(CASE WHEN station_decade = '201' AND napa_decade = '2010' THEN weather_sim ELSE NULL END) AS weather_sim_2010
+			AVG(CASE WHEN station_decade = '200' THEN weather_sim ELSE NULL END) AS weather_sim_2000, 
+			AVG(CASE WHEN station_decade = '201' THEN weather_sim ELSE NULL END) AS weather_sim_2010
 		FROM weather_sim1 
+		WHERE napa_decade = '2010'
 		GROUP BY station_id) t1 
 	LEFT JOIN (
 		SELECT station_id, 
-			AVG(CASE WHEN station_decade = '201' AND napa_decade = '2010' THEN weather_sim ELSE NULL END) AS weather_sim_future 	
+			AVG(CASE WHEN station_decade = '201' THEN weather_sim ELSE NULL END) AS weather_sim_future 	
 		FROM weather_sim2
+		WHERE napa_decade = '2010' 
 		GROUP BY station_id) t2 
 	ON t1.station_id = t2.station_id
-	WHERE weather_sim_2000 IS NOT NULL AND weather_sim_2010 IS NOT NULL;  
+	WHERE weather_sim_2000 IS NOT NULL AND weather_sim_2010 IS NOT NULL AND weather_sim_future IS NOT NULL;  
